cdfca00f6517e8d79625b4bc9a4e69f6ca9929f8
Jassen Castillo <castillo.cristianj@gmail.com>
2023-11-05 17:11:33 -0300

    fix(middleware.ts): make middleware function async to support using await inside feat(middleware.ts): add JWT verification to check if user is authenticated, redirect to homepage if not

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { verify } from "jsonwebtoken";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
     const cookies = request.cookies;
     const auth = cookies.get("auth")
 
     if (!auth) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+    try {
+        const user = await verify(`${auth?.value}`, "secret") as any
+    } catch (error) {
         return NextResponse.redirect(new URL('/', request.url))
     }
     return NextResponse.next()
