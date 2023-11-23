cd483f07db8968e2287ca2a12e8aa0e9abe20ddc
Jassen Castillo <castillo.cristianj@gmail.com>
2023-11-23 13:02:49 -0300

    fix(app/admin/page.tsx): remove unused code and fix indentation for better readability feat(app/admin/page.tsx): add support for creating banks and users through API calls fix(components/atoms/SinaUser/index.tsx): add caching headers to API request for getting user profile fix(components/molecules/SinaAppBar/index.tsx): add caching headers to API request for getting user profile fix(components/molecules/SinaUserModal/index.tsx): add caching headers to API request for logging out fix(custom-hooks/useGetBanks.tsx): add caching headers to API request for getting banks data fix(custom-hooks/useGetProfile.tsx): add caching headers to API request for getting user profile fix(lib/pfinales.prisma.ts): add caching headers to API requests for fetching declarations and beneficiaries by date and rut respectively

--- src/custom-hooks/useGetBanks.tsx
+++ src/custom-hooks/useGetBanks.tsx
@@ -1,35 +1,36 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
-export const useGetBanks = (load?:any) => {
+export const useGetBanks = (load?: any) => {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
   const getBanks = async () => {
     setIsLoading(true);
-    try{
-      const { data } = await axios.get("/api/banks/getBanks",{
-        headers:{
-          'Cache-Control': 'no-cache',
-          'Pragma': 'no-cache',
-          'Expires': '0',
-        }
-      });
+    try {
+      const config = {
+        headers: {
+          "Cache-Control": "no-cache, no-store, must-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
+      const { data } = await axios.get("/api/banks/getBanks", config);
       const dataParsed = JSON.parse(data);
       //console.log(dataParsed)
       setData(dataParsed);
     }
-    catch(error){
+    catch (error) {
       setIsError(error as any);
     }
-    finally{
+    finally {
       setIsLoading(false);
     }
   }
-  useEffect(()=>{
-      getBanks();
-  },[])
+  useEffect(() => {
+    getBanks();
+  }, [])
 
-  return {isLoading, isError, data}
+  return { isLoading, isError, data }
 }
