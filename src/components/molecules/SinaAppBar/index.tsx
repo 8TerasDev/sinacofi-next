4b2250afdd3aab89d7073963386be71c51b6be56
Yanny Hernandez <yanny.hernandez@leniolabs.com>
2023-11-23 13:14:45 -0300

    Added set path

--- src/components/molecules/SinaAppBar/index.tsx
+++ src/components/molecules/SinaAppBar/index.tsx
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react";
-import Image from 'next/image'
+import Image from "next/image";
 import styles from "./sinaappbar.module.css";
 import { Button, Stack } from "@mui/material";
-import sinacofi_logo from '../../../assets/images/sinacofi_logo.png'
+import sinacofi_logo from "../../../assets/images/sinacofi_logo.png";
 import { Person } from "@mui/icons-material";
 import axios from "axios";
+import { setBasePath } from "@/contexts/path.context";
 
-
-const SinaAppBar = ({handleAdmin}:any) => {
+const SinaAppBar = ({ handleAdmin }: any) => {
   const [isAdmin, setIsAdmin] = useState(false);
   const getProfile = async () => {
-    try{
+    try {
       const { data } = await axios.get("/api/auth/getprofile");
       setIsAdmin(data.user.isAdmin);
+    } catch (error) {
+      console.log(error);
     }
-    catch(error){
-      console.log(error)
-    }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
+    setBasePath(document.location.pathname);
     getProfile();
-  },[])
-   
+  }, []);
+
   return (
     <div className={styles.sinappbar_container}>
-      {isAdmin &&       
-        <Stack padding={'5px'}>
-          <Button 
-          startIcon={<Person/>}
-          onClick={handleAdmin} variant="contained" color="success">
+      {isAdmin && (
+        <Stack padding={"5px"}>
+          <Button
+            startIcon={<Person />}
+            onClick={handleAdmin}
+            variant='contained'
+            color='success'
+          >
             Administrador
           </Button>
         </Stack>
-      }
-      <Stack alignItems={'flex-end'} width={'100%'}>
-        <Image src={sinacofi_logo} alt="" width={180} /> 
+      )}
+      <Stack alignItems={"flex-end"} width={"100%"}>
+        <Image src={sinacofi_logo} alt='' width={180} />
       </Stack>
     </div>
   );
