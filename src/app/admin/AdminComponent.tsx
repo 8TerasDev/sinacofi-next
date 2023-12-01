44e71ae4a0ee2bfc63da310bcc6d211310cd9469
maueli-leniolabs <mauro.elias@leniolabs.com>
2023-12-01 12:55:13 -0300

    disable users disabling bank

--- src/app/admin/AdminComponent.tsx
+++ src/app/admin/AdminComponent.tsx
@@ -338,8 +338,6 @@ const AdminPage = () => {
     }
   };
 
-
-
   const updateBankData = (_row: any) => {
     const data: any[] = [...(bankDataList || banksData || [])];
     const prev: any = data[_row.id];
@@ -358,7 +356,7 @@ const AdminPage = () => {
       try {
         return await callback(args);
       } catch (ex: any) {
-        debugger;
+        // debugger;
         setError(ex.message);
       }
     };
@@ -375,6 +373,22 @@ const AdminPage = () => {
   const disableBank = errorWrapper(async (row: any) => {
     await deleteBankById(row?._id);
     updateBankData({ ...row, status: "DISABLED" });
+
+    //@ts-ignore
+    const newUsers = usersData && usersData.map(async(user:any) => {
+      if(user.bank_id == row._id){
+        await deleteUserById(user.id);
+        return {...user, status: "DISABLED" }
+      }
+      return user
+    })
+    //@ts-ignore
+    Promise.all(newUsers).then((results):any=>{
+      //@ts-ignore
+      setUserDataList(results)
+    })
+
+
   });
   const disableUser = errorWrapper(async (row: any) => {
     await deleteUserById(row?._id);
