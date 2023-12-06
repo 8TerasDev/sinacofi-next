f5875c622a91aa557c9db9275a97bdafeba4eafe
Jorge Soto <jorge.soto@leniolabs.com>
2023-12-06 12:35:07 -0400

    fix: add validation and masking to Phone inputs

--- src/components/organisms/EditBankForm/index.tsx
+++ src/components/organisms/EditBankForm/index.tsx
@@ -1,4 +1,5 @@
 import { CreateFormsProps } from '@/app/admin/page';
+import PhoneInputMask from '@/components/atoms/PhoneInputMask';
 import { FormControl, Stack, Grid, TextField, Button } from '@mui/material';
 import React from 'react';
 
@@ -7,66 +8,69 @@ export const EditBankForm = ({
   setOpenModal,
   currentRow,
 }: CreateFormsProps) => {
-  const {nombre, codigo} = currentRow; 
-  return(
-    <FormControl 
+  const { nombre, codigo } = currentRow;
+  return (
+    <FormControl
       fullWidth
       required
       component={'form'}
       onSubmit={handleSubmit}
-      sx={{ justifyContent:'space-between', flex:1}}
-      >
+      sx={{ justifyContent: 'space-between', flex: 1 }}
+    >
       <Stack overflow={'auto'}>
-        <Grid container sx={{ justifyContent:'center', height:'100%', flex:1}} >
+        <Grid container sx={{ justifyContent: 'center', height: '100%', flex: 1 }} >
           <Grid item sm={4} padding={'10px'}>
             <TextField
               defaultValue={nombre}
-              required 
+              required
               variant='filled'
               label='Nombre'
               placeholder='Nombre'
-              sx={{width:'100%'}}
-              />  
+              sx={{ width: '100%' }}
+            />
           </Grid>
           <Grid item sm={4} padding={'10px'}>
             <TextField
               defaultValue={codigo}
-              required 
+              required
               variant='filled'
               label='Codigo'
               placeholder='Codigo'
-              sx={{width:'100%'}}
-              />  
+              sx={{ width: '100%' }}
+            />
           </Grid>
           <Grid item sm={4} padding={"10px"}>
             <TextField
               required
               variant='filled'
-              label={'Telefono'}
-              placeholder={'Telefono'}
+              label={'Teléfono'}
+              placeholder={'Teléfono'}
               sx={{ width: "100%" }}
               type='tel'
+              InputProps={{
+                inputComponent: PhoneInputMask as any,
+              }}
             />
           </Grid>
         </Grid>
-    </Stack>
-    <Stack padding={'10px'} flexDirection={'row'} justifyContent={'space-around'}>
-      <Button
-        sx={{width:'40%'}}
-        color='success'
-        variant='contained' 
-        type='submit'>
-        EDITAR
-      </Button>
-      <Button
-        sx={{width:'40%'}}
-        fullWidth
-        color='inherit'
-        variant='contained' 
-        onClick={()=>setOpenModal(false)}>
-        Cerrar
-      </Button>
-    </Stack>
-  </FormControl>
+      </Stack>
+      <Stack padding={'10px'} flexDirection={'row'} justifyContent={'space-around'}>
+        <Button
+          sx={{ width: '40%' }}
+          color='success'
+          variant='contained'
+          type='submit'>
+          EDITAR
+        </Button>
+        <Button
+          sx={{ width: '40%' }}
+          fullWidth
+          color='inherit'
+          variant='contained'
+          onClick={() => setOpenModal(false)}>
+          Cerrar
+        </Button>
+      </Stack>
+    </FormControl>
   )
 }
